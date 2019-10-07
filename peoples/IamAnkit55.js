f32b5397e55b4e292f857b580ef8b457261b5250
Ankit Mehta <54588691+IamAnkit55@users.noreply.github.com>
2019-10-07 13:49:42 +0530

    Create IamAnkit55.js
    Added my data in IamAnkit55.js

---
+++ peoples/IamAnkit55.js
@@ -0,0 +1,9 @@
+  
+module.exports = {
+  name: 'Ankit Mehta',
+  github: 'IamAnkit55',
+  email: 'ankitmehta55@gmail.com',
+  twitter: '@Ankit_Mehta_',
+  facebook: 'iamankit55',
+  linkedin: 'in/iamankit55'
+}
